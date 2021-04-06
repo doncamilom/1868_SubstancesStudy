569db9b8ec96c068c03ac7794fa55ac317c28e4a
doncamilom <doncamilom@gmail.com>
2021-04-06 11:58:51 -0500

    New code to convert the results from OnlyStrs into python usual format

--- Code/OnlyStrs/runAll.sh
+++ Code/OnlyStrs/runAll.sh
@@ -3,13 +3,13 @@
 STARTTIME=$(date +%s.%N)
 
 # First run perl code that changes data format
-perl format.pl $1 > perlFormatted.tsv
+perl format.pl $1 > ./Data/perlFormatted.tsv
 
 # Then run awk code to find all possible substitution formulas
-awk -f getAllRs.awk perlFormatted.tsv > allRs_dirty.tsv
+awk -f getAllRs.awk ./Data/perlFormatted.tsv > ./Data/allRs_dirty.tsv
 
 # Finally, run awk code to find all non unique Rs + relationships between elements.
-awk -f nonUniqRs.awk allRs_dirty.tsv > allRs_clean.tsv
+awk -f nonUniqRs.awk ./Data/allRs_dirty.tsv > ./Data/allRs_clean.tsv
 
 ENDTIME=$(date +%s.%N)
 echo "Elapsed time: $( echo "$ENDTIME - $STARTTIME" | bc -l ) seconds"
